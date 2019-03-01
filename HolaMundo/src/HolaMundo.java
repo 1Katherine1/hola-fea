d19a35d73c007616de6e4969aa0fb2859c5fcc78
rafaelpolo19 <rafaelpolo@cbit-online.com>
2019-03-01 07:03:04 -0800

    Primera versión Hola fea
    Fea

---
+++ HolaMundo/src/HolaMundo.java
@@ -0,0 +1,28 @@
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		System.out.println("Hola Javier León");
+		System.out.println("Rafael xd");
+
+		System.out.println("JavierElDesarrollador");
+
+		System.out.println("Rafael MAS");
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
+		System.out.println("4");
+		System.out.println("5");
+		System.out.println("7");
+		System.out.println("8");
+		
+		System.out.println("a");
+		System.out.println("b");
+		System.out.println("c");
+		System.out.println("e");
+		System.out.println("f");
+
+
+	}
+
+}
